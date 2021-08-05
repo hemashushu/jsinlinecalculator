5db00b0d5b4e54f334f9e23f15da474b4842fd7a
Hippo Spark (Arch) <hippospark@gmail.com>
2021-08-06 06:48:51 +0800

    增加了比较和逻辑运算。

--- src/inlinecalculator.js
+++ src/inlinecalculator.js
@@ -20,7 +20,7 @@ const factorial = (n) => {
 };
 
 parser.yy.functions = {
-    factorial: factorial,
+    fact: factorial, // 阶乘
 
     // 大部分函数直接使用 JavaScript 的内置 Math 对象提供的功能，
     // Math 对象支持的函数见这里：
@@ -48,17 +48,7 @@ parser.yy.functions = {
 /**
  * 简单的单行算式求值模块
  *
- * 支持的功能：
- *
- * - 正负整数或小数，如： 3, -6, 3.14, -0.618；
- * - 加减乘除余运算（即 + - * / %）；
- * - 指数运算（幂运算），如： 3^2 = 9, 2^10 = 1024；
- * - 阶乘，如 3! = 6, 4! = 24；
- * - 括号改变运算优先级，如： (1+2) * 3 = 9；
- * - 对数运算，如：log10(100) = 2, log2(1024) = 10, ln(E) = 1, log(10,100) = 2，其中 log(base, n) 表示以 base 为底的 n 的对数；
- * - 常用函数：绝对值 `abs`、开方 `sqrt`、四舍五入 `round`、取整 `trunc`，如：abs(-12) = 12, sqrt(4) = 2, round(3.14) = 3, round(2.718) = 3, trunc(3.14) = 3, trunc(2.718) = 2；
- * - 圆周率常数 π（即 PI ）和欧拉常数/自然常数 e（即 E），如 2 * PI = 6.28；
- * - 三角函数正弦 `sin`, 余弦 `cos`, 正切 `tan` 以及反正弦 `asin`, 反余弦 `acos`, 反正切 `atan`， 它们的参数的单位是 "弧度"。如：sin(PI/2) = 1, cos(0) = 1, tan(PI/4) = 1, asin(1) = PI/2, acos(1) = 0, atan(1) = PI/4。
+ * 支持的功能包括四则运算、常用函数、比较、逻辑、位运算等。
  *
  */
 class InlineCalculator {
